159977d573d5b712e9a96f10dff6871be05ec806
Evan Goulden <evangoulden@Evans-MacBook-Pro-2.local>
2019-02-20 13:27:48 +0000

    Removed unnecesary lists and loops

--- get_ios_info.py
+++ get_ios_info.py
@@ -1,38 +1,35 @@
-from netmiko import ConnectHandler
 from napalm import get_network_driver
-from pprint import pprint
-
-file = open('devices', 'r')
-
-supported_os_types = ['ios', 'asa']
-
-all_device_list = []
-
-for line in file:
-    device_info_list = line.strip().split(',')
-    device_info = {}
-    device_info['name'] = device_info_list[0]
-    device_info['ip'] = device_info_list[1]
-    device_info['os'] = device_info_list[2]
-    device_info['username'] = device_info_list[3]
-    device_info['password'] = device_info_list[4]
-
-    all_device_list.append(device_info)
-
-for device in all_device_list:
-    if device['os'] in supported_os_types:
-        get_network_driver(device['os'])
-        driver = get_network_driver(device['os'])
-        device = driver(device['ip'],
-                        device['username'],
-                        device['password'])
-        device.open()
-        facts = device.get_facts()
-        print('Hostname: ' + facts['hostname'])
-        print('Vendor: ' + facts['vendor'])
-        print('Software Version: ' + facts['os_version'])
-        print('Device Model: ' + facts['model'])
-        print('Serial Number: ' + facts['serial_number'])
-        device.close()
-    else:
-        print("\n *** " + device['name'] + ' is not an supported device. Skipping.')
+ 
+def main():
+    device = {}
+    with open('/Users/evangoulden/Documents/Python Projects/iMac Project/devices.csv', 'r') as devices_data:
+        for lines in devices_data:
+            line = lines.split(sep=',')
+            device['name'] = line[0]
+            device['ip_address'] = line[1]
+            device['fqdn'] = line[2]
+            device['os'] = line[3]
+            device['username'] = line[4]
+            device['password'] = line[5]
+            supported_devices = ['ios', 'procurve']
+            if device['os'] in supported_devices:
+                driver = get_network_driver(device['os'])
+                connected_device = driver(device['ip_address'], device['username'], device['password'])
+                try:
+                    connected_device.open()
+                    facts = connected_device.get_facts()
+ 
+                except:
+                    print("Could not connect to the specified device, check your connection...")
+ 
+                print(facts['vendor'])
+                print(facts['os_version'])
+                print(facts['model'])
+                print(facts['serial_number'])
+ 
+            else:
+                print("The device is not of a supported type, please try a supported type.")
+ 
+   
+if __name__ == "__main__":
+    main()
